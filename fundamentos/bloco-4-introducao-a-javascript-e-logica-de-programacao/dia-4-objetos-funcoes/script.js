0afcd506da09da93329f764475620a0010a9b8be
Arthur Araujo <technoasti@gmail.com>
2022-04-11 23:41:39 -0300

    Realizando a impressão de dois objetos concatenados, ex. 5

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-funcoes/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-objetos-funcoes/script.js
@@ -4,12 +4,25 @@ let info = {
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim'
+}
 
-console.log('Bem-vinda, ' + info.personagem);
+//console.log('Bem-vinda, ' + info.personagem);
 
 info.recorrente= 'Sim';
 
-console.log(info);
+console.log(info.personagem + ' e ' + info2.personagem);
+console.log(info.origem + ' e ' + info2.origem);
+console.log(info.nota + ' e ' + info2.nota);
+if(info.recorrente === 'Sim' && info2.recorrente === 'Sim') {
+  console.log('Ambos recorrentes');
+}
+
+/*console.log(info);
 
 for (key in info) {
   console.log(key);
@@ -17,4 +30,4 @@ for (key in info) {
 
 for (key in info) {
   console.log(info[key]);
-}
+}*/
