8a56c3a9ab7447a2c6d88a84ebdcbc6e8b9cc3ce
Arthur Araujo <technoasti@gmail.com>
2022-04-19 11:41:29 -0300

    Adicionando evento de click no calendário

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -0,0 +1,52 @@
+function createDaysOfTheWeek() {
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  };
+};
+
+createDaysOfTheWeek();
+
+// Escreva seu código abaixo.
+
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+let catUl = document.querySelector('#days');
+
+for (let i = 0; i < dezDaysList.length; i += 1) {
+  let dayList = dezDaysList[i];
+
+  let listLine = document.createElement('li');
+  listLine.innerHTML = dayList
+  catUl.appendChild(listLine);
+  listLine.classList.add('day')
+  if (dayList === 24 || dayList === 25 || dayList === 31) {
+    listLine.classList.add('holiday');
+  }
+  if (dayList === 4 || dayList === 11 || dayList === 18 || dayList === 25) {
+    listLine.classList.add('friday')
+  }
+}
+function hollidays(feriados) {
+
+}
+let buttons = document.querySelector('.buttons-container');
+let buttonHoliday = document.createElement('button');
+buttons.appendChild(buttonHoliday);
+buttonHoliday.setAttribute('id', 'btn-holiday');
+document.querySelector('#btn-holiday').innerHTML = "Feriados";
+
+const catHolidays = document.querySelector('#btn-holiday');
+const classHoliday = document.querySelector('.holiday');
+catHolidays.addEventListener("click", clickHolidays)
+
+function clickHolidays(){
+  classHoliday.style.backgroundColor = "green"
+  classHoliday.style.color = "white"
+}
