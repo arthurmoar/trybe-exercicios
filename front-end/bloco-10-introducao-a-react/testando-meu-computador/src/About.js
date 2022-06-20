3fca6e100c2c7356f6d49b017a1904bd0d30025d
Arthur Araujo <technoasti@gmail.com>
2022-06-20 14:21:50 -0300

    Exericios do dia 10.1

---
+++ front-end/bloco-10-introducao-a-react/testando-meu-computador/src/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          { "Arthur Araujo" }
+        </h1>
+        <p>
+          { "Olá, meu nome é Arthur tenho 31 anos, sou casado com uma mulher maravilhosa e tenho dois filhos incríveis, estou estudando na Trybe e me esforçando muito para ser um excelente desenvolvedor." }
+        </p>
+      </div>
+    )
+  }
+}
+
+export default About;
